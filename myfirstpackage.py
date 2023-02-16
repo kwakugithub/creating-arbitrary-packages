2751f4842726b627ee91fb385aa3dbefef029325
Opare <kwaku.opare@scidom.de>
2023-02-16 13:55:53 +0100

    my first commit

---
+++ myfirstpackage.py
@@ -0,0 +1,12 @@
+def myfirstmodule1():
+    print("do i need to create subdirectories1")
+
+def myfirstmodule2():
+    print("do i need to create subdirectories2")
+
+def myfirstmodule3():
+    print("do i need to create subdirectories3")
+
+
+
+    
---
+++ mysecondpackage.py
@@ -0,0 +1,12 @@
+def mysecondmoduel():
+    print("this is my second directories1")
+
+def mysecondmodue2():
+    print("this is my second directories2")
+
+    
+def mysecondmodue3():
+    print("this is my second directories3")
+
+
+  
