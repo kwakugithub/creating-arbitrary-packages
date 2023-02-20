5457fd76e901543e31867fe66a2b01117201a344
kwaku opare <110134459+kwakugithub@users.noreply.github.com>
2023-02-20 17:13:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,19 +5,10 @@ setup(
     version="0.1",
     description="creating arbitrary package",
     author='kwaku Opare',
-    author_email='kwakuoparefavor@gmail.com',
-<<<<<<< HEAD
+    author_email='kwakuoparefavor@gmail.com
     install_requires=[],
     python_requires=">=3.10",
     packages=find_packages()
     
 
 )
-=======
-    keywords='arbitrary'
-    packages='setuptools.find_packages()'
-    install_requires=['']
-)
-
-
->>>>>>> ec397336c864e2ce395dd6ee5b25b6f4d0f3a712
